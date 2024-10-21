dbb138c1050ea59514ba338c767eacbd97a82b7e
SiemenWilmaertsPXL <11601448@student.pxl.be>
2024-10-21 10:58:16 +0200

    finished

---
+++ VeelgebruikteKlassen1 Raadspel/VeelgebruikteKlassen1 Raadspel/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VeelgebruikteKlassen1_Raadspel
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int randomNumber;
+        int counter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void endButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void evaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(numberTextBox.Text, out int inputNumber);
+
+            if (inputNumber < randomNumber)
+            {
+                counter++;
+                output1TextBox.Text = "Raad Hoger";
+            }
+            if (inputNumber > randomNumber)
+            {
+                counter++;
+                output1TextBox.Text = "Raad lager";
+            }
+            if (inputNumber==randomNumber){
+                output1TextBox.Text = "Proficiat! U hebt het getal geraden";
+                output2TextBox.Text = $"Aantal keren geraden: {counter}";
+
+            }
+        }
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            var random= new Random();
+            randomNumber = random.Next(0, 100);
+            counter = 0;
+        }
+    }
+}
